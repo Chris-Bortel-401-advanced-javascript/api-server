dc0325a437c877e878519608d41b87acbf7e9851
Chris Bortel <bortelcc19@gmail.com>
2020-09-07 15:29:14 -0700

    testing

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 
 require('dotenv').config();
 
+const mongoose = require('mongoose');
 let server = require('./lib/server.js');
 
+const MONGODB_URI = 'mongodb://localhost:27017/class-08';
+
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(MONGODB_URI, mongooseOptions);
 server.start(process.env.PORT);
